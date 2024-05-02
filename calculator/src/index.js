4f65ef2d399224474c5db055147fbc6d0b15bf4e
Alexandre Telles <alexandre.ntr@gmail.com>
2024-05-02 10:33:18 +0000

    base layout

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import Calculator from './main/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Calculator />
+    <div>
+      <h1>Calculator</h1>
+      <Calculator />
+    </div>
   </React.StrictMode>
 );
